c0c6488fc32564648127e7e67f0a8fe09dd1f7c0
Shivaniraj05 <86128017+Shivaniraj05@users.noreply.github.com>
2024-01-19 23:35:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 const byteSize = (str) => {
   // write your code here
+	if (str === '') {
+        return 0;
+    }
+	var blob = new Blob([str]);
+
+    // Get the size of the Blob in bytes
+    var sizeInBytes = blob.size;
+
+    return sizeInBytes;
+	
 };
 
 // Do not change the code below
